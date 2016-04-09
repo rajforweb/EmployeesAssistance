add3b00e30ab9170002d3940a3c0e3e17be31ad4
ranjth <ranjithvikram@gmail.com>
2016-04-09 12:45:13 -0400

    skeleton for GeoLocationApi & Category api

---
+++ EmployeeAssistance.Api/Controllers/CategoryController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeAssistance.Api.Models;
+
+namespace EmployeeAssistance.Api.Controllers
+{
+    public class CategoryController : ApiController
+    {
+      
+        
+    }
+}
---
+++ EmployeeAssistance.Api/Controllers/GeoLocationController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeAssistance.Api.Controllers
+{
+    public class GeoLocationController : ApiController
+    {
+
+    }
+}
