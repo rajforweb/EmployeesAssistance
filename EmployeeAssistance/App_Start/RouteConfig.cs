2da28817c3b1e3085ddd20b9987b1e3eab698994
rajforweb <raj@RUDRA>
2016-04-09 14:55:14 -0700

    test

--- EmployeeAssistance/App_Start/RouteConfig.cs
+++ EmployeeAssistance/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace EmployeeAssistance
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Reader", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
