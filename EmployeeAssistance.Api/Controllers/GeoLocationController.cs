7a5eb3bd2ceda298471e822cd5da4ca1d4d0d6ae
ranjth <ranjithvikram@gmail.com>
2016-04-09 12:50:24 -0400

    added resultmodel

--- EmployeeAssistance.Api/Controllers/GeoLocationController.cs
+++ EmployeeAssistance.Api/Controllers/GeoLocationController.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using EmployeeAssist.WebApi.Models;
+using EmployeeAssist.WebApi.Providers;
+using EmployeeAssist.WebApi.Results;
+using System.Linq;
+using EmployeeAssistance.Api.Models;
 
-namespace EmployeeAssistance.Api.Controllers
+namespace EmployeeAssist.WebApi.Controllers
 {
+
     public class GeoLocationController : ApiController
     {
 
