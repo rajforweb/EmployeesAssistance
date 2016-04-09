d672af173d4501f0fbf9b691b47c2178fd0de0cc
ranjth <ranjithvikram@gmail.com>
2016-04-09 15:08:27 -0400

    editor

---
+++ EmployeeAssistance.Api/Controllers/EditorController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.OAuth;
+using EmployeeAssistance.Api.Models;
+using System.Linq;
+using EmployeeAssistance.DataAccess;
+using MongoDB.Bson;
+
+
+namespace EmployeeAssist.WebApi.Controllers
+{
+    public class EditorController : ApiController
+    {
+        // GET: Editor
+        [Route("api/Create")]
+        [HttpPost]
+        public void Create([FromBody]EditorModel model)
+        {
+            IMongoDAL dal = new MongoDAL();
+            dal.Insert(model.Country, model.State, model.City, model.Category, model.SubCategory, 0, model.Information[0].Description,System.DateTime.Now);
+        }
+
+        // GET: Editor
+        [Route("api/Update")]
+        [HttpPost]
+        public void Update([FromBody]EditorModel model)
+        {
+            IMongoDAL dal = new MongoDAL();
+            dal.LIK
+            //dal.li(model.Country, model.State, model.City, model.Category, model.SubCategory, model.Likes, model.Descrption);
+        }
+
+    }
+}
---
+++ EmployeeAssistance.Api/Controllers/ReaderController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using EmployeeAssistance.Api.Models;
+using EmployeeAssistance.DataAccess;
+using MongoDB.Bson;
+
+namespace EmployeeAssist.WebApi.Controllers
+{
+    public class ReaderController : ApiController
+    {
+        // GET: Editor
+        [Route("api/Read")]
+        [HttpGet]
+        public List<BsonDocument> GetAllRecords([FromBody]CategoryModel model)
+        {
+            IMongoDAL dal = new MongoDAL();
+            var result = dal.GetInformation(model.Country, model.State, model.City, model.Category, model.SubCategory);
+            return result;
+        }
+
+    }
+}
