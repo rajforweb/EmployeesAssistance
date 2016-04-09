65037c9d751bff1c3ebeddcc30b1a2f5009a9e60
Rahul_Damle <Rahul_Damle@USABOS125152L>
2016-04-09 12:38:15 -0400

    Added DAL skeleton

---
+++ EmployeeAssistance.DataAccess/IMongoDAL.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAssistance.DataAccess
+{
+    interface IMongoDAL
+    {
+        List<BsonDocument> GetInformation(string country, string state, string city, string category, string subcategory);
+        void Insert(string country, string state, string city, string category, string subcategory, int Likes, string Description);
+        void InsertCategorySubCategory(string category, string subcategory);
+        List<BsonDocument> GetCategorySubCategory();
+        List<BsonDocument> GetSubCategory(string category);
+    }
+}
---
+++ EmployeeAssistance.DataAccess/MongoDAL.cs
@@ -0,0 +1,42 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAssistance.DataAccess
+{
+    public class MongoDAL : IMongoDAL
+    {
+        List<BsonDocument> IMongoDAL.GetInformation(string country, string state, string city, string category, string subcategory)
+        {
+           return null;
+        }
+
+        List<BsonDocument> IMongoDAL.GetCategorySubCategory()
+        {
+            return null;
+        }
+
+        List<BsonDocument> IMongoDAL.GetSubCategory(string Category)
+        {
+            return null;
+        }
+               
+
+        void IMongoDAL.Insert(string country, string state, string city, string category, string subcategory, int Likes, string Description)
+        {
+            
+        }
+
+
+        void IMongoDAL.InsertCategorySubCategory(string category, string subcategory)
+        {
+            
+        }
+
+    }
+
+}
+}
